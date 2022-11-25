c3564dba37a9da1a8de268676d7ef8cdba05fbbc
2453852330 <53853822+2453852330@users.noreply.github.com>
2022-11-25 14:24:04 +0800

    Update SocketExample.cpp
    add example for handle function

--- SocketHelper/Source/SocketHelper/Private/SocketExample.cpp
+++ SocketHelper/Source/SocketHelper/Private/SocketExample.cpp
@@ -5,5 +5,12 @@
 
 void ASocketExample::CF_ReceiveData(TArray<uint8> buffer, int32 size)
 {
+	// call this function to let the code run in game thread;
+	CF_HandleFunction();
+}
+
+void ASocketExample::CF_HandleFunction()
+{
+	// you can do any handle in there for game thread;
 	UE_LOG(LogTemp,Warning,TEXT("[SocketExample][%d][receive data size = {%d}]"),CF_GetPort(),size);
 }
